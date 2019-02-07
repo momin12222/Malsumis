19465cf98a8ca59357381482522b8812d11f5845
Ruthenium <rutaspk@gmail.com>
2019-02-07 20:16:54 +0100

    Looping background

---
+++ Malsumis/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    Movement movementScript;
+
+    public float length;
+
+    void Start()
+    {
+        movementScript = GetComponent<Movement>();
+    }
+
+    void Update()
+    {
+        movementScript.MoveForward();
+
+        if (transform.position.x < -length)
+        {
+            LoopBackground();
+        }
+    }
+
+    private void LoopBackground()
+    {
+        Vector2 groundOffSet = new Vector2(length * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffSet;
+    }
+
+}
