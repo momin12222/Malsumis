0331f830f7d528cc17ec2f0d8cb9bf30c696f22a
Momin Chowdhury <Mominmiah.Chowdhury.4432@student.uu.se>
2019-03-18 16:13:07 +0100

    Added Fade script

---
+++ Malsumis/Assets/Scripts/UIFader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFader : MonoBehaviour{
+
+    public CanvasGroup uiElement;
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 1));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 0));
+    }
+
+    public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float lerpTime = 0.5f)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStared = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStared / lerpTime;
+
+
+        while(true)
+        {
+            timeSinceStared = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStared / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.alpha = currentValue;
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        print("done");
+    }
+}
